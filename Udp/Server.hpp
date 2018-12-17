8db5f3f9570ba7f22aadb7bf2f6fa931936d090c
Amour132 <1046692639@qq.com>
2018-12-17 15:08:36 +0800

    frist submit

---
+++ Udp/Server.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string>
+
+class Sock
+{
+  public:
+    Sock()
+    {}
+    SockInit()
+    {
+      
+    }
+    Recv();
+    Send();
+    ~Sock();
+  private:
+    Sock _sock;
+};
+
